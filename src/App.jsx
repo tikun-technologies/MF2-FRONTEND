e38dc3964f0bb5dc7ab39a727772ce7f4bd4d3f3
J Brown <j.b.fitterman@gmail.com>
2025-02-28 18:09:22 +0000

    Added redirection from home page to dashboard if user logged in. Added token throughout application

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./components/common/Navbar/Navbar";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Home from "./pages/Home/Home";
@@ -12,15 +12,31 @@ import ProtectedRoute from "./middlewares/ProtectedRoute";
 
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css"; // Import Toast styles
-import { AuthProvider } from "./context/AuthContext";
+import AuthContext, { AuthProvider } from "./context/AuthContext";
+import { useContext } from "react";
+
 function App() {
+  return (
+    <AuthProvider>
+      <MainApp />
+    </AuthProvider>
+  );
+}
+
+function MainApp() {
+  const { token } = useContext(AuthContext);
+  console.log("Token from root app:", token);
   return (
     <>
       {" "}
       {/* All app has access to token */}
       <AuthProvider>
         <Routes>
-          <Route path="/" element={<Home />} />
+          {" "}
+          <Route
+            path="/"
+            element={token ? <Navigate to="/dashboard" /> : <Home />}
+          />
           {/* ✅ Use middleware to protect dashboard */}
           <Route element={<ProtectedRoute />}>
             <Route path="/dashboard" element={<Dashboard />} />
@@ -34,5 +50,29 @@ function App() {
     </>
   );
 }
+
+// function App() {
+//   const { token } = useContext(AuthContext);
+//   console.log("Token reached in app:", token);
+//   return (
+//     <>
+//       {" "}
+//       {/* All app has access to token */}
+//       <AuthProvider>
+//         <Routes>
+//           <Route path="/" element={<Home />} />
+//           {/* ✅ Use middleware to protect dashboard */}
+//           <Route element={<ProtectedRoute />}>
+//             <Route path="/dashboard" element={<Dashboard />} />
+//           </Route>
+//           <Route path="/signup" element={<Register />} />
+//           <Route path="/create" element={<AddDataForm />} />
+//           <Route path="/study/:id" element={<StudyDetail />} />
+//           <Route path="/login" element={<Login />} />
+//         </Routes>
+//       </AuthProvider>
+//     </>
+//   );
+// }
 
 export default App;
