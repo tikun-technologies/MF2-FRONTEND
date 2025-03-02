45b74d740f97de03b1eae75bb77910c8a0a79553
J Brown <j.b.fitterman@gmail.com>
2025-03-02 13:04:18 +0000

    Created data / test and moved articles test data to it

---
+++ src/data/test/articlesData.js
@@ -0,0 +1,68 @@
+export const articlesData = [
+  {
+    _id: 1,
+    title: "Keir Stamer pounds Zelensky",
+    description:
+      "Zelensky met Keir Stamer at 10 downing and he absolutely got pounded. We have never seen a pounding like this",
+    date: "Feb 25, 2025",
+    readTime: "5 mins",
+    category: "Research",
+    image:
+      "https://media.cnn.com/api/v1/images/stellar/prod/2025-03-01t174617z-1698872933-rc2h4dapo419-rtrmadp-3-ukraine-crisis-britain.jpg?c=16x9&q=h_653,w_1160,c_fill/f_webp",
+  },
+  {
+    _id: 2,
+    title: "Keir Stamer pounds Zelensky",
+    description:
+      "Zelensky met Keir Stamer at 10 downing and he absolutely got pounded. We have never seen a pounding like this",
+    date: "Feb 25, 2025",
+    readTime: "5 mins",
+    category: "Research",
+    image:
+      "https://media.cnn.com/api/v1/images/stellar/prod/2025-03-01t174617z-1698872933-rc2h4dapo419-rtrmadp-3-ukraine-crisis-britain.jpg?c=16x9&q=h_653,w_1160,c_fill/f_webp",
+  },
+  {
+    _id: 3,
+    title: "Keir Stamer pounds Zelensky",
+    description:
+      "Zelensky met Keir Stamer at 10 downing and he absolutely got pounded. We have never seen a pounding like this",
+    date: "Feb 25, 2025",
+    readTime: "5 mins",
+    category: "Research",
+    image:
+      "https://media.cnn.com/api/v1/images/stellar/prod/2025-03-01t174617z-1698872933-rc2h4dapo419-rtrmadp-3-ukraine-crisis-britain.jpg?c=16x9&q=h_653,w_1160,c_fill/f_webp",
+  },
+  {
+    _id: 4,
+    title: "Keir Stamer pounds Zelensky",
+    description:
+      "Zelensky met Keir Stamer at 10 downing and he absolutely got pounded. We have never seen a pounding like this",
+    date: "Feb 25, 2025",
+    readTime: "5 mins",
+    category: "Research",
+    image:
+      "https://media.cnn.com/api/v1/images/stellar/prod/2025-03-01t174617z-1698872933-rc2h4dapo419-rtrmadp-3-ukraine-crisis-britain.jpg?c=16x9&q=h_653,w_1160,c_fill/f_webp",
+  },
+  {
+    _id: 5,
+    title: "Keir Stamer pounds Zelensky",
+    description:
+      "Zelensky met Keir Stamer at 10 downing and he absolutely got pounded. We have never seen a pounding like this",
+    date: "Feb 25, 2025",
+    readTime: "5 mins",
+    category: "Research",
+    image:
+      "https://media.cnn.com/api/v1/images/stellar/prod/2025-03-01t174617z-1698872933-rc2h4dapo419-rtrmadp-3-ukraine-crisis-britain.jpg?c=16x9&q=h_653,w_1160,c_fill/f_webp",
+  },
+  {
+    _id: 6,
+    title: "Keir Stamer pounds Zelensky",
+    description:
+      "Zelensky met Keir Stamer at 10 downing and he absolutely got pounded. We have never seen a pounding like this",
+    date: "Feb 25, 2025",
+    readTime: "5 mins",
+    category: "Research",
+    image:
+      "https://media.cnn.com/api/v1/images/stellar/prod/2025-03-01t174617z-1698872933-rc2h4dapo419-rtrmadp-3-ukraine-crisis-britain.jpg?c=16x9&q=h_653,w_1160,c_fill/f_webp",
+  },
+];
--- src/pages/Home/ArticlesSection.jsx
+++ src/pages/Home/ArticlesSection.jsx
@@ -2,78 +2,9 @@ import React from "react";
 import ArticleCard from "../../components/Cards/ArticleCard";
 import styles from "./Home.module.css";
 import { FiArrowRight } from "react-icons/fi";
-FiArrowRight;
+import { articlesData } from "../../data/test/articlesData";
 
 const ArticlesSection = () => {
-  const articlesData = [
-    {
-      _id: 1,
-      title: "Keir Stamer pounds Zelensky",
-      description:
-        "Zelensky met Keir Stamer at 10 downing and he absolutely got pounded. We have never seen a pounding like this",
-      date: "Feb 25, 2025",
-      readTime: "5 mins",
-      category: "Research",
-      image:
-        "https://media.cnn.com/api/v1/images/stellar/prod/2025-03-01t174617z-1698872933-rc2h4dapo419-rtrmadp-3-ukraine-crisis-britain.jpg?c=16x9&q=h_653,w_1160,c_fill/f_webp",
-    },
-    {
-      _id: 2,
-      title: "Keir Stamer pounds Zelensky",
-      description:
-        "Zelensky met Keir Stamer at 10 downing and he absolutely got pounded. We have never seen a pounding like this",
-      date: "Feb 25, 2025",
-      readTime: "5 mins",
-      category: "Research",
-      image:
-        "https://media.cnn.com/api/v1/images/stellar/prod/2025-03-01t174617z-1698872933-rc2h4dapo419-rtrmadp-3-ukraine-crisis-britain.jpg?c=16x9&q=h_653,w_1160,c_fill/f_webp",
-    },
-    {
-      _id: 3,
-      title: "Keir Stamer pounds Zelensky",
-      description:
-        "Zelensky met Keir Stamer at 10 downing and he absolutely got pounded. We have never seen a pounding like this",
-      date: "Feb 25, 2025",
-      readTime: "5 mins",
-      category: "Research",
-      image:
-        "https://media.cnn.com/api/v1/images/stellar/prod/2025-03-01t174617z-1698872933-rc2h4dapo419-rtrmadp-3-ukraine-crisis-britain.jpg?c=16x9&q=h_653,w_1160,c_fill/f_webp",
-    },
-    {
-      _id: 4,
-      title: "Keir Stamer pounds Zelensky",
-      description:
-        "Zelensky met Keir Stamer at 10 downing and he absolutely got pounded. We have never seen a pounding like this",
-      date: "Feb 25, 2025",
-      readTime: "5 mins",
-      category: "Research",
-      image:
-        "https://media.cnn.com/api/v1/images/stellar/prod/2025-03-01t174617z-1698872933-rc2h4dapo419-rtrmadp-3-ukraine-crisis-britain.jpg?c=16x9&q=h_653,w_1160,c_fill/f_webp",
-    },
-    {
-      _id: 5,
-      title: "Keir Stamer pounds Zelensky",
-      description:
-        "Zelensky met Keir Stamer at 10 downing and he absolutely got pounded. We have never seen a pounding like this",
-      date: "Feb 25, 2025",
-      readTime: "5 mins",
-      category: "Research",
-      image:
-        "https://media.cnn.com/api/v1/images/stellar/prod/2025-03-01t174617z-1698872933-rc2h4dapo419-rtrmadp-3-ukraine-crisis-britain.jpg?c=16x9&q=h_653,w_1160,c_fill/f_webp",
-    },
-    {
-      _id: 6,
-      title: "Keir Stamer pounds Zelensky",
-      description:
-        "Zelensky met Keir Stamer at 10 downing and he absolutely got pounded. We have never seen a pounding like this",
-      date: "Feb 25, 2025",
-      readTime: "5 mins",
-      category: "Research",
-      image:
-        "https://media.cnn.com/api/v1/images/stellar/prod/2025-03-01t174617z-1698872933-rc2h4dapo419-rtrmadp-3-ukraine-crisis-britain.jpg?c=16x9&q=h_653,w_1160,c_fill/f_webp",
-    },
-  ];
-
   return (
     <section className={styles.articlesSection}>
       <div className={`container ${styles.articlesContainer}`}>
