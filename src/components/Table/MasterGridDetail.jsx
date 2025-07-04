7858402537297384b1b2a37b38b2c61f87761ea7
J Brown <j.b.fitterman@gmail.com>
2025-07-04 22:02:09 +0530

    integrated AG grid with test data

--- src/components/Table/MasterGridDetail.jsx
+++ src/components/Table/MasterGridDetail.jsx
@@ -1,7 +1,153 @@
-import React from "react";
+import React, { useState, useEffect, useMemo } from "react";
+import { AllCommunityModule, ModuleRegistry } from "ag-grid-community";
+import { themeAlpine } from "ag-grid-community";
+
+import { colorSchemeDarkBlue } from "ag-grid-community";
+
+// 3. Import the React Data Grid Component
+import { AgGridReact } from "ag-grid-react"; // React Data Grid Component
+import { studyTestData } from "./StudyTestData";
+
+// 4. Define the rows and columns
 
 const MasterGridDetail = () => {
-  return <div>MasterGridDetail</div>;
+  // Row Data: The data to be displayed.
+  const [rowData, setRowData] = useState([]);
+  const [colDefs, setColDefs] = useState([]);
+
+  const theme = themeAlpine.withPart(colorSchemeDarkBlue).withParams({
+    fontFamily: "Anek Devanagari",
+    headerFontFamily: "Anek Devanagari",
+    cellFontFamily: "Anek Devanagari",
+    selectedRowBackgroundColor: "rgba(0, 255, 0, 0.1)",
+    // color and style of border around selection
+    rangeSelectionBorderColor: "rgb(193, 0, 97)",
+    rangeSelectionBorderStyle: "dashed",
+    // background color of selection - you can use a semi-transparent color
+    // and it wil overlay on top of the existing cells
+    rangeSelectionBackgroundColor: "rgb(255, 0, 128, 0.1)",
+    // color used to indicate that data has been copied form the cell range
+    rangeSelectionHighlightColor: "rgb(60, 188, 0, 0.3)",
+
+    // alternating row colors will be visible through the semi-transparent
+    // selection background color
+    oddRowBackgroundColor: "#8881",
+  });
+
+  console.log("Alpine properties: ", theme);
+
+  const rowSelection = useMemo(() => {
+    return { mode: "multiRow" };
+  }, []);
+
+  useEffect(() => {
+    if (studyTestData && studyTestData.length > 0) {
+      setRowData(studyTestData);
+
+      const firstRow = studyTestData[0];
+      const columns = Object.keys(firstRow)
+        .filter((key) => key !== "") // optionally filter out empty keys
+        .map((key, index) => ({
+          field: key,
+          headerName: key,
+          sortable: true,
+          filter: true,
+          wrapText: true,
+          autoHeight: true,
+          cellStyle: {
+            whiteSpace: "nowrap",
+            overflow: "hidden",
+            textOverflow: "wrap",
+          },
+          cellRenderer:
+            key === "Successful"
+              ? (params) => (params.value ? "✔️" : "❌")
+              : (params) => {
+                  const val = params.value;
+
+                  // ✅ Only apply style for numeric values > 20
+                  if (typeof val === "number" && val > 20) {
+                    return (
+                      <span
+                        style={{
+                          backgroundColor: "#198754",
+                          color: "white",
+                          paddingTop: "4px",
+                          paddingLeft: "8px",
+                          paddingRight: "8px",
+                          borderRadius: "32px",
+                          fontWeight: 600,
+                          fontSize: "0.8rem",
+                          display: "inline-flex",
+                          alignItems: "center",
+                          justifyContent: "center",
+                          minWidth: "24px",
+                          height: "16px",
+                          lineHeight: "1",
+                          whiteSpace: "nowrap",
+                        }}
+                      >
+                        {val.toLocaleString()}
+                      </span>
+                    );
+                  }
+
+                  // Otherwise, just return plain value
+                  return (
+                    <span
+                      style={{
+                        // backgroundColor: "#198754",
+                        color: "white",
+
+                        paddingLeft: "8px",
+                        paddingRight: "8px",
+                        borderRadius: "999px",
+                        // fontWeight: 600,
+                        fontSize: "0.8rem",
+                        display: "inline-flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        minWidth: "24px",
+                        height: "10px",
+                        lineHeight: "1",
+                        whiteSpace: "nowrap",
+                      }}
+                    >
+                      {val.toLocaleString()}
+                    </span>
+                  );
+                },
+          tooltipField: key,
+        }));
+
+      setColDefs(columns);
+    }
+  }, []);
+
+  //   const defaultColDef = {
+  //     flex: 1,
+  //   };
+
+  // // 5. React Data Grid Component
+  return (
+    // Data Grid will fill the size of the parent container
+    <div style={{ width: "100%", height: "800px" }}>
+      <AgGridReact
+        theme={theme}
+        rowSelection={rowSelection}
+        rowData={rowData}
+        columnDefs={colDefs}
+        pagination={true} // Enable Pagination
+        // rowSelection="multiple"
+        cellSelection={true}
+        enableCharts={true}
+        // rowHeight={40}
+        // defaultColDef={defaultColDef}
+      />
+    </div>
+  );
+
+  // ...
 };
 
 export default MasterGridDetail;
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,23 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
 import "./index.css";
 import App from "./app/app.jsx";
+import { ModuleRegistry } from "ag-grid-community";
+import {
+  AllEnterpriseModule,
+  LicenseManager,
+  IntegratedChartsModule,
+} from "ag-grid-enterprise";
+import { AgChartsEnterpriseModule } from "ag-charts-enterprise";
+
+ModuleRegistry.registerModules([
+  AllEnterpriseModule,
+  IntegratedChartsModule.with(AgChartsEnterpriseModule),
+]);
+
+LicenseManager.setLicenseKey(
+  "[TRIAL]_this_{AG_Charts_and_AG_Grid}_Enterprise_key_{AG-088973}_is_granted_for_evaluation_only___Use_in_production_is_not_permitted___Please_report_misuse_to_legal@ag-grid.com___For_help_with_purchasing_a_production_key_please_contact_info@ag-grid.com___You_are_granted_a_{Single_Application}_Developer_License_for_one_application_only___All_Front-End_JavaScript_developers_working_on_the_application_would_need_to_be_licensed___This_key_will_deactivate_on_{31 July 2025}____[v3]_[0102]_MTc1MzkxNjQwMDAwMA==6e64d6974b07ee1c751d36c6da13e44a"
+);
 
 createRoot(document.getElementById("root")).render(
   // <StrictMode>
